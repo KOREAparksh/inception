b82737e20288328d9d9676f2adb31ef4bf9e6d47
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2022-09-02 22:09:35 +0900

    add: docker setting

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-sudo curl -sSL https://get.docker.com/ | sh
-
-sudo chmod 666 /var/run/docker.sock
-
-sudo su -c 'printf "{\n\t\"live-restore\": true\n}" > /etc/docker/docker.json'
-
-sudo curl -L https://github.com/docker/compose/releases/download/v2.10.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-
+sudo curl -sSL https://get.docker.com/ | sh && \
+sudo curl -L https://github.com/docker/compose/releases/download/v2.10.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && \
+sudo systemctl restart docker && \
+sudo su -c 'printf "{\n\t\"live-restore\": true\n}" > /etc/docker/docker.json' && \
+sudo chmod 666 /var/run/docker.sock && \
 sudo chmod +x /usr/local/bin/docker-compose
 
-sudo systemctl restart docker
+echo "alias del-container=\"docker rm -f `docker ps -qa`\"" >> ~/.zshrc
+echo "alias del-image=\"docker rmi -f $(docker images -aq)\"" >> ~/.zshrc
+
+source ~/.zshrc
--- scripts/init.sh
+++ scripts/init.sh
@@ -3,24 +3,39 @@
 # 1. sudo apt-get install -y git
 # 2. git clone https://github.com/nfl1ryxditimo12/inception
 
-sudo sed -i "s/http:\/\/security.ubuntu.com/https:\/\/mirror.kakao.com" /etc/apt/sources.list
-sudo sed -i "s/http:\/\/kr.archive.ubuntu.com/https:\/\/mirror.kakao.com" /etc/apt/sources.list
+function repository() {
+  sudo sed -i "s/http:\/\/security.ubuntu.com/https:\/\/mirror.kakao.com" /etc/apt/sources.list
+  sudo sed -i "s/http:\/\/kr.archive.ubuntu.com/https:\/\/mirror.kakao.com" /etc/apt/sources.list
+}
+
+function zsh() {
+  sudo apt-get install -y curl
+
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  sudo sed -i "s/plugins=(git)/plugins=(git zsh-autosuggestions)"
+  source ~/.zshrc
+}
+
+function ssh() {
+  sudo apt-get install -y \
+    inet-tools \
+    openssh-server \
+
+  sudo sed -i "s/#Port 22/Port 22" /etc/ssh/sshd_config
+  sudo systemctl restart ssh
+  sudo ufw enable
+  sudo ufw allow 22
+}
+
+function util() {
+  sudo apt-get install -y \
+    apt-transport-https \
+    make \
+    vim \
+    systemd \
+}
 
 sudo apt-get update
 
-## SSH Setting
-
-sudo apt-get install \
-	inet-tools \
-	openssh-server \
-
-sudo sed -i "s/#Port 22/Port 22" /etc/ssh/sshd_config
-sudo systemctl restart ssh
-sudo ufw enable
-sudo ufw allow 22
-
-sudo apt-get install -y \
-	apt-transport-https \
-	make \
-	vim \
-	systemd \
+repository && zsh && ssh && util
