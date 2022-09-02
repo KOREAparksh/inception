81a6cd58a257357857212268e7ff4dc9ca1377e0
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2022-09-02 20:18:03 +0900

    refactor: docker, add docker-compose installation

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-sh get-docker.sh && \
-	sudo chmod 666 /var/run/docker.sock && \
-	sudo chmod +x /usr/local/bin/docker-compose && \
-	sudo su -c 'printf "{\n\t\"live-restore\": true\n}" > /etc/docker/docker.json' && \
-	sudo systemctl restart docker
+sudo curl -sSL https://get.docker.com/ | sh
+
+sudo chmod 666 /var/run/docker.sock
+
+sudo su -c 'printf "{\n\t\"live-restore\": true\n}" > /etc/docker/docker.json'
+
+sudo curl -L https://github.com/docker/compose/releases/download/v2.10.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo systemctl restart docker
