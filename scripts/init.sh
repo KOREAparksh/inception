1077a062a13ec1e673d6773aefd5ed43e71b28c6
Seunghan Park <seunpark@student.42seoul.kr>
2022-09-04 15:20:25 +0900

    zsh 설치 추가

--- scripts/init.sh
+++ scripts/init.sh
@@ -13,7 +13,7 @@ function repository() {
 
 function zsh() {
 	echo "=================================================ZSH START"
-  sudo apt-get install -y curl
+  sudo apt-get install -y curl zsh
 
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
