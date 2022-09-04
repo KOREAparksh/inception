fb9f8993b35cc3f1d0d08eb1465633958ebbed53
Seunghan Park <seunpark@student.42seoul.kr>
2022-09-04 14:36:14 +0900

    스크립트 syntax error (/ in util, sed not file in zsh)

--- scripts/init.sh
+++ scripts/init.sh
@@ -13,7 +13,7 @@ function zsh() {
 
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-  sudo sed -i "s/plugins=(git)/plugins=(git zsh-autosuggestions)/g"
+  sudo sed -i "s/plugins=(git)/plugins=(git zsh-autosuggestions)/g" ~/.zshrc
   source ~/.zshrc
 }
 
@@ -33,7 +33,7 @@ function util() {
     apt-transport-https \
     make \
     vim \
-    systemd \
+    systemd
 }
 
 sudo apt-get update
