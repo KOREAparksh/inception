894dd2bb546c77a70eb3a88c145eecb4ebb52d97
Seunghan Park <seunpark@student.42seoul.kr>
2022-09-04 17:45:15 +0900

    docker.sh alias 수정2

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -7,8 +7,8 @@ sudo su -c 'printf "{\n\t\"live-restore\": true\n}" > /etc/docker/docker.json' &
 sudo chmod 666 /var/run/docker.sock && \
 sudo chmod +x /usr/local/bin/docker-compose
 
-echo "alias del-container=\"docker rm -f \`docker images -qa\`" >> ~/.zshrc
-echo "alias del-image=\"docker rmi -f \`docker images -qa\`" >> ~/.zshrc
+echo "alias del-container=\"docker rm -f \`docker images -qa\`\"" >> ~/.zshrc
+echo "alias del-image=\"docker rmi -f \`docker images -qa\`\"" >> ~/.zshrc
 
 source ~/.zshrc
 
